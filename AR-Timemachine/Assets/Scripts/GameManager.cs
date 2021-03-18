d149d8f94867fe9414c1d1ad080cff48490e7eaa
OfficialPath <42738205+OfficialPath@users.noreply.github.com>
2021-03-17 18:42:02 -0600

    scripts updated
    - ImageReferenceLibrary added
    - each image added
    TODO:
    - missing logo

---
+++ AR-Timemachine/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class GameManager : MonoBehaviour
+{
+    public XRReferenceImageLibrary myLibrary;
+
+    void Awake()
+    {
+        var manager = gameObject.AddComponent<ARTrackedImageManager>();
+        manager.referenceLibrary = myLibrary;
+        manager.enabled = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaceSelector() //jamie's function
+    {
+        
+    }
+}
---
+++ AR-Timemachine/Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageTracking : MonoBehaviour
+{
+    public ARTrackedImageManager manager;
+    public XRReferenceImageLibrary myLibrary;
+    public XRImageTrackingSubsystem subsystem;
+
+
+
+
+    void Awake()
+    {
+        subsystem.imageLibrary = myLibrary;
+        subsystem.Start();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
