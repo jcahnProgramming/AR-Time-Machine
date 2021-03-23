a68b19bff255277198fd6799b7dbf256c8cf3398
OfficialPath <42738205+OfficialPath@users.noreply.github.com>
2021-03-22 19:13:48 -0600

    imagetracking update

--- AR-Timemachine/Assets/Scripts/ImageTracking.cs
+++ AR-Timemachine/Assets/Scripts/ImageTracking.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 public class ImageTracking : MonoBehaviour
 {
-    public ARTrackedImageManager manager;
+    //public ARTrackedImageManager manager;
     public XRReferenceImageLibrary myLibrary;
     public XRImageTrackingSubsystem subsystem;
-
+    public Button startScanning;
 
 
 
@@ -28,6 +29,17 @@ public class ImageTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if (startScanning)
+        {
+            ScanForLogo();
+        }
+    }
+
+    void ScanForLogo()
+    {
+        if (myLibrary[4].texture)
+        {
+            //when logo has been identified, then move to next step
+        }
     }
 }
